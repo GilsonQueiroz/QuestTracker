80977af980eac7681737553b96cec11ab6bc4928
Gilson Queiroz <gilsontq@yahoo.com>
2022-09-14 12:54:44 -0300

    alteração objeto Task

--- Tasks.js
+++ Tasks.js
@@ -2,14 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var taskSchema = new Schema({
-    slug:String,
-    quest:String,
-    task:String,
-    codquest:String,
-    local:String,
-    day:Number,
-    time:String,
-    complete:Boolean
+    slug: String,
+    quest: String,
+    task: String,
+    codquest: String,
+    local: String,
+    day: Number,
+    time: String,
+    complete: Boolean,
+    disponible: Boolean
 },{collection:'tasks'});
 
 var Tasks = mongoose.model("Tasks",taskSchema);
